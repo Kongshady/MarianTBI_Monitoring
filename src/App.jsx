6fa7e54d0e5ed9a357b4774b0b8fd09f6926b2cb
Mikko Jardenico <mjardenico_210000001699@gmail.com>
2025-01-31 19:44:29 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Welcome from './Welcome.jsx'
+
+function App() {
+  
+  return(
+    <>
+      <Welcome />
+    </>
+  );
+}
+
+export default App
---
+++ src/Welcome.jsx
@@ -0,0 +1,10 @@
+
+
+function Welcome(){
+    
+    return(
+        <h1>Hello Welcome to Hell!!</h1>
+    );
+}
+
+export default Welcome
