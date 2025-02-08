3d08843a9b8ca207905fdbf7f5ce0946209387b6
Mikko Jardenico <mjardenico_210000001699@gmail.com>
2025-02-08 21:22:44 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Loginform from './components/LoginForm.jsx'
+
+function App() {
+  return(
+    <Loginform />
+  );
+}
+
+export default App
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,11 @@
+
+
+function Loginform(){
+    return(
+        <div className="h-screen flex items-center justify-center ">
+            Does this work
+        </div>
+    );
+}
+
+export default Loginform
